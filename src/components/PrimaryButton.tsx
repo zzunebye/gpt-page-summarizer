e213fac07c3a86a06d8e24c4729aa38ab2295843
zzunebye <bhang96@gmail.com>
2023-04-06 22:45:02 +0800

    add PrimaryButton as separate component

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from "preact";
+
+const PrimaryButton: FunctionComponent<PrimaryButtonProps> = ({
+  isLoading,
+  onClick,
+  children,
+}) => {
+  return (
+    <button
+      class={`w-full mb-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded ${
+        isLoading ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+type PrimaryButtonProps = {
+  isLoading: boolean;
+  onClick: () => void;
+  children: any;
+};
+
+export default PrimaryButton;
